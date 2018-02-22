ca2ec2617fbd72f0d6b82c1dac7439c1398ab8b2
marina <marina.shatinskaya@gmail.com>
2018-02-22 12:10:34 +0300

    Task 2. Love triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+   var a, b, c, counter=0;
+
+  for ( i=0; i<preferences.length; i++ ){
+    if (typeof (preferences [i]) == "number"){
+      a = preferences [i];
+      b = preferences [a-1];
+      c = preferences [b-1];
+      if (( i+1==c ) && ((a!==b) && (b!==c))){
+        counter ++;
+        preferences [i] = undefined;
+        preferences [a-1] = undefined;
+        preferences [b-1] = undefined;
+      }
+    }
+  }
+  return counter;
 };
